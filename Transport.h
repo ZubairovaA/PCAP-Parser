c217c2ae5efb8640c9395f8e62385a83a2ee9bc3
ZubairovaA <41435166+ZubairovaA@users.noreply.github.com>
2021-01-26 19:43:47 +1000

    Update Transport.h

--- Transport.h
+++ Transport.h
@@ -10,15 +10,14 @@ using namespace std;
 
 class Internet_ip;
 
+class Transport_tcp  {
 
-class Transport_tcp {
-
-	unsigned short src_port;	// номер порта отправителя                 source port
-	unsigned short dst_port;	// номер порта получателя                  destination port
-	 unsigned long th_seq;		// номер пакета в последовательности       sequence number
-	 unsigned long th_ack;		// номер подтверждения                     acknowledgement number
-	unsigned char th_offx2;	    // длина заголовка (4 бита) TCP_Length = (tcp->th_offx2) >>4     data offset
-	unsigned char th_flags;     //флаги    flags
+	unsigned short src_port;	// РЅРѕРјРµСЂ РїРѕСЂС‚Р° РѕС‚РїСЂР°РІРёС‚РµР»СЏ                 source port
+	unsigned short dst_port;	// РЅРѕРјРµСЂ РїРѕСЂС‚Р° РїРѕР»СѓС‡Р°С‚РµР»СЏ                  destination port
+	 unsigned long th_seq;		// РЅРѕРјРµСЂ РїР°РєРµС‚Р° РІ РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚Рё       sequence number
+	 unsigned long th_ack;		// РЅРѕРјРµСЂ РїРѕРґС‚РІРµСЂР¶РґРµРЅРёСЏ                     acknowledgement number
+	unsigned char th_offx2;	    // РґР»РёРЅР° Р·Р°РіРѕР»РѕРІРєР° (4 Р±РёС‚Р°) TCP_Length = (tcp->th_offx2) >>4     data offset
+	unsigned char th_flags;     //С„Р»Р°РіРё    flags
 #define TH_FIN 0x01
 #define TH_SYN 0x02
 #define TH_RST 0x04
@@ -28,47 +27,53 @@ class Transport_tcp {
 #define TH_ECE 0x40
 #define TH_CWR 0x80
 #define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-	unsigned short th_win;		//окно                      window
-	unsigned short sum;		    //чексумма                  checksum
-	unsigned short th_urp;		//экстренный указатель      urgent pointer
+	unsigned short th_win;		//РѕРєРЅРѕ                      window
+	unsigned short sum;		    //С‡РµРєСЃСѓРјРјР°                  checksum
+	unsigned short th_urp;		//СЌРєСЃС‚СЂРµРЅРЅС‹Р№ СѓРєР°Р·Р°С‚РµР»СЊ      urgent pointer
 
-public:
 	Transport_tcp (){}
 	 ~Transport_tcp() {}
-	void Show_TL(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX);
+	//void Show_TL(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX) const;
+void Show_TL(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX, bool& To_Continue) const;
+      friend class Internet_ip;
+	  friend class  TL_ptoto_type;
 };
 
 class Transport_udp  {
-	unsigned short   src_port;       // номер порта отправителя 
-	unsigned short   dst_port;       // номер порта получателя 
-	unsigned short   udp_length;     // длина датаграммы 
-	unsigned short   sum;            //чексумма
-public:
+	unsigned short   src_port;       // РЅРѕРјРµСЂ РїРѕСЂС‚Р° РѕС‚РїСЂР°РІРёС‚РµР»СЏ 
+	unsigned short   dst_port;       // РЅРѕРјРµСЂ РїРѕСЂС‚Р° РїРѕР»СѓС‡Р°С‚РµР»СЏ 
+	unsigned short   udp_length;     // РґР»РёРЅР° РґР°С‚Р°РіСЂР°РјРјС‹ 
+	unsigned short   sum;            //С‡РµРєСЃСѓРјРјР°
 	Transport_udp (){}
 	 ~Transport_udp (){}
-	void Show_TL(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX);
+	//void Show_TL(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX) const;
+	 void Show_TL(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX, bool& To_Continue) const;
+	 friend class Internet_ip;
+	 friend class  TL_ptoto_type;
 };
 
 class Transport_ICMP  {
-	unsigned char   ICMP_type;           // тип ICMP- пакета
-	unsigned char   ICMP_code;           // код ICMP- пакета 
-	unsigned short  sum;                 //чексумма
+	unsigned char   ICMP_type;           // С‚РёРї ICMP- РїР°РєРµС‚Р°
+	unsigned char   ICMP_code;           // РєРѕРґ ICMP- РїР°РєРµС‚Р° 
+	unsigned short  sum;                 //С‡РµРєСЃСѓРјРјР°
 	union {
-		struct { unsigned char  uc1, uc2, uc3, uc4; } s_uc;  //переменная часть заголовка
+		struct { unsigned char  uc1, uc2, uc3, uc4; } s_uc;  //РїРµСЂРµРјРµРЅРЅР°СЏ С‡Р°СЃС‚СЊ Р·Р°РіРѕР»РѕРІРєР°
 		struct { unsigned short us1, us2; } s_us;
 		unsigned long s_ul;
 	} s_icmp;
-public:
+
 	Transport_ICMP (){}
 	 ~Transport_ICMP (){}
-	 void Show_TL(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX, bool& To_Continue);
+	 void Show_TL(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX, bool& To_Continue) const;
+	 friend class Internet_ip;
+	 friend class  TL_ptoto_type;
 };
 
 
 
 
 
-class Port_type {               //контейнер для поиска типа протокола прикладного уровня
+class Port_type {               //РєРѕРЅС‚РµР№РЅРµСЂ РґР»СЏ РїРѕРёСЃРєР° С‚РёРїР° РїСЂРѕС‚РѕРєРѕР»Р° РїСЂРёРєР»Р°РґРЅРѕРіРѕ СѓСЂРѕРІРЅСЏ
 public:
 	map<unsigned short, string> mp_port;
 	void fill_port();
