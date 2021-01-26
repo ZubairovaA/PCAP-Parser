0b5bed901f4380920fbf47a6b31691a52bc43d75
ZubairovaA <41435166+ZubairovaA@users.noreply.github.com>
2021-01-26 22:10:39 +1000

    Update Internet.h

--- Internet.h
+++ Internet.h
@@ -17,24 +17,21 @@ public:
 	unsigned short ip_len;		   // общий размер всего пакета   the size of the packet
 	unsigned short ip_id;		   // идентификационный номер пакета при разбивке файла на части   identification
 	unsigned short ip_off;		   // fragment offset field   
-#define IP_RF 0x8000		       // флаг- зарезервированный фрагмент    reserved fragment flag 
-#define IP_DF 0x4000		       // флаг- можно ли фрагментировать      don't fragment flag
-#define IP_MF 0x2000		       // флаг- будут и еще фрагменты         more fragments flag
-#define IP_OFFMASK 0x1fff	       // маска для фрагментирования битов    mask for fragmenting bits
+#define IP_RF 0x8000		           // флаг- зарезервированный фрагмент    reserved fragment flag 
+#define IP_DF 0x4000		           // флаг- можно ли фрагментировать      don't fragment flag
+#define IP_MF 0x2000		           // флаг- будут и еще фрагменты         more fragments flag
+#define IP_OFFMASK 0x1fff	           // маска для фрагментирования битов    mask for fragmenting bits
 	unsigned char ip_ttl;		   // time to live
-	unsigned char ip_p;		       // протокол след уровня                next level protocol
+	unsigned char ip_p;		   // протокол след уровня                next level protocol
 	unsigned short ip_sum;		   // чексумма                            checksum
-	unsigned char ip_src[4];       //                                     source address
-	unsigned char ip_dst[4];       //                                     dest address
+	unsigned char ip_src[4];           // source address
+	unsigned char ip_dst[4];           // dest address
 
 	Internet_ip() {};
 	~Internet_ip() {};
 	void Write_IP_Addr(ofstream& Parse_File);
 	int ip_size();   //the size of the internet layer header
-
-   
-    void Check_TL(ofstream& Parse_File, const unsigned char *TP_Hdr, const char* payload,  unsigned short& AppProtocol, bool& Is_FIX, bool& To_Continue);
-
+        void Check_TL(ofstream& Parse_File, const unsigned char *TP_Hdr, const char* payload,  unsigned short& AppProtocol, bool& Is_FIX, bool& To_Continue);
 };
 
 
@@ -47,7 +44,8 @@ public:
 
     template <class T> struct Builder : public Base   //the class to create the object of the transport layer protocol class
     {
-        void build(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX, bool& To_Continue) {
+        void build(const unsigned char* TP_Hdr, const char* payload, ofstream& Parse_File, Internet_ip* ip, unsigned short& AppProtocol, bool& Is_FIX, bool& To_Continue)
+	{
             T* p= new T();
             p = (T*)(TP_Hdr);    //initialize the pointer to the new class with the pointer to the beginning of the transoprt layer header in the packet.  
             p->Show_TL( TP_Hdr,  payload,  Parse_File,  ip,  AppProtocol, Is_FIX, To_Continue); // all the work with the transport layer header.
@@ -59,7 +57,6 @@ public:
     template <class T>
     void add(const string& name);   // addind new transoprt layers protocols to map
 
-   
     void fill_TL();  // filling the map
 
     TL_ptoto_type() {
